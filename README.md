74fe8a89467ade594ebb97298e7b2f5c862f7159
Camila Oliveira <milow.amu@gmail.com>
2026-07-06 14:33:33 -0300

    Roteiro parte 5

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## Site em execução
 
 ![Página do usuário](tela_do_usuario.png)
-
-![Página do admin](tela_do_admin.png)
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.text import slugify
+from django.contrib import messages
 
 # Create your views here.
-from .models import Mensagem
-
+from .forms import MensagemForm
+from .models import Mensagem, Tag
 
 def index(request):
     mensagens = Mensagem.objects.all()
@@ -10,3 +12,23 @@ def index(request):
 
 def sobre(request):
     return render(request, "home/sobre.html")
+
+def nova_mensagem(request):
+    if request.method == "POST":
+        form = MensagemForm(request.POST)
+        if form.is_valid():
+            mensagem = form.save()
+
+            tags_texto = form.cleaned_data["tags"]
+            for pedaco in tags_texto.split(","):
+                nome = slugify(pedaco)
+                if nome:
+                    tag, _ = Tag.objects.get_or_create(nome=nome)
+                    mensagem.tags.add(tag)
+                messages.success(request, "Mensagem publicada com sucesso!")
+
+            return redirect("index")
+    else:
+        form = MensagemForm()
+
+    return render(request, "home/nova.html", {"form": form})
